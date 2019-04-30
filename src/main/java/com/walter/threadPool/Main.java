c14514a9ac64d8e568c962336e480163c09772c9
walter52 <walter52@sohu.com>
2019-04-30 10:01:30 +0800

    '调整为maven项目'

--- src/main/java/com/walter/threadPool/Main.java
+++ src/main/java/com/walter/threadPool/Main.java
@@ -12,7 +12,6 @@ public class Main {
       threadPoolManager.execute(() ->
           System.out.println(Thread.currentThread().getName()));
     }
-    int a=0;
   }
 
 }
---
+++ src/test/java/threadPool.java
@@ -0,0 +1,9 @@
+/**
+ * @author super
+ * @date 2019/4/30
+ */
+public class threadPool {
+
+  
+
+}
