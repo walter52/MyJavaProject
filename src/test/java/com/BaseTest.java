bdce2f503e67de289bff55ccefa5f065f0591c55
walter52 <walter52@sohu.com>
2019-04-30 11:46:31 +0800

    测试方法补全

---
+++ src/test/java/com/BaseTest.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author super
+ * @date 2019/4/30
+ */
+public class BaseTest {
+  protected static final Logger LOG = LoggerFactory.getLogger(BaseTest.class);
+
+  public BaseTest(){
+    LOG.info("-------test start------");
+  }
+
+}
--- src/test/java/com/threadpool/ThreadPoolManagerTest.java
+++ src/test/java/com/threadpool/ThreadPoolManagerTest.java
@@ -1,5 +1,6 @@
-package threadpool;
+package com.threadpool;
 
+import com.BaseTest;
 import com.walter.threadpool.ThreadPoolManager;
 import org.junit.Test;
 
@@ -7,14 +8,15 @@ import org.junit.Test;
  * @author super
  * @date 2019/4/30
  */
-public class ThreadPoolManagerTest {
+public class ThreadPoolManagerTest extends BaseTest {
+
 
   @Test
   public void test(){
       ThreadPoolManager threadPoolManager = ThreadPoolManager.getInstacne();
       for(int i=4;i>0;i--){
         threadPoolManager.execute(() ->
-            System.out.println(Thread.currentThread().getName()));
+            LOG.info(Thread.currentThread().getName()));
       }
   }
 
