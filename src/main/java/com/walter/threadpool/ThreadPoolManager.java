e73969ebda76d0e431ae68cb766a1aefa584e67c
walter52 <walter52@sohu.com>
2019-04-30 15:01:28 +0800

    补充有返回值的线程池调用方法

--- src/main/java/com/walter/threadpool/ThreadPoolManager.java
+++ src/main/java/com/walter/threadpool/ThreadPoolManager.java
@@ -1,41 +1,80 @@
 package com.walter.threadpool;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor.AbortPolicy;
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author super
  * @date 2019/4/29
  */
- public class ThreadPoolManager<T> {
+public class ThreadPoolManager<T> {
 
-  private static volatile  ThreadPoolManager instance;
+  private static volatile ThreadPoolManager instance;
 
+  /**
+   * 采用手动创建线程池
+   */
   private ExecutorService threadPool = new ThreadPoolExecutor(3, 3, 0L, TimeUnit.MILLISECONDS,
-      new LinkedBlockingQueue<Runnable>());
+      new LinkedBlockingQueue<Runnable>(),
+      new ThreadFactoryBuilder().setNameFormat("demo-pool-%d").build(), new AbortPolicy());
 
   private ThreadPoolManager() {
   }
 
- public static ThreadPoolManager getInstacne(){
-    if (instance == null){
-      synchronized (ThreadPoolManager.class){
-        if (instance == null){
+  public static ThreadPoolManager getInstacne() {
+    if (instance == null) {
+      synchronized (ThreadPoolManager.class) {
+        if (instance == null) {
           instance = new ThreadPoolManager();
         }
       }
     }
     return instance;
- }
+  }
 
- public void execute(Runnable r){
+  /**
+   * 开启无返回结果的线程
+   *
+   * @param r Runnable
+   */
+  public void execute(Runnable r) {
 
-    threadPool.submit(r);
- }
+    threadPool.execute(r);
+  }
 
+  /**
+   * 开启有返回结果的线程
+   *
+   * @param c Callable
+   * @return 多线程返回结果
+   */
+  public Future<T> submit(Callable<T> c){
+    return threadPool.submit(c);
+  }
 
-
+  /**
+   * 等待线程执行结束
+   *
+   * @param
+   * @return
+   */
+  public void awaitForShutdown(){
+    threadPool.shutdown();
+    try {
+      if (!threadPool.awaitTermination(60, TimeUnit.SECONDS)) {
+        threadPool.shutdownNow();
+      }
+    } catch (InterruptedException ex) {
+      threadPool.shutdownNow();
+      Thread.currentThread().interrupt();
+    }
+  }
 
 }
--- src/test/java/com/threadpool/ThreadPoolManagerTest.java
+++ src/test/java/com/threadpool/ThreadPoolManagerTest.java
@@ -2,6 +2,10 @@ package com.threadpool;
 
 import com.BaseTest;
 import com.walter.threadpool.ThreadPoolManager;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -14,12 +18,33 @@ public class ThreadPoolManagerTest extends BaseTest {
 
 
   @Test
-  public void test(){
+  public void executeTest(){
       ThreadPoolManager threadPoolManager = ThreadPoolManager.getInstacne();
       for(int i=4;i>0;i--){
         threadPoolManager.execute(() ->
             log.info(Thread.currentThread().getName()));
       }
   }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void submitTest(){
+    ThreadPoolManager threadPoolManager = ThreadPoolManager.getInstacne();
+    List<String> list = new ArrayList<>();
+    try{
+      for(int i=4;i>0;i--){
+        Future<String> future = threadPoolManager.submit(()->Thread.currentThread().getName());
+        String str = future.get();
+        list.add(str+":success");
+      }
+    }catch (Exception e){
+      log.error("线程异常",e);
+    }
+
+    threadPoolManager.awaitForShutdown();
+    list.forEach(log::info);
+
+  }
+
 
 }
